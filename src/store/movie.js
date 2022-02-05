14265bf01e01b0d5ba5b68acb2918edd2f7b56a7
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-02-05 19:19:14 +0900

    Add Netlify functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import _uniqBy from 'lodash/uniqBy';
 
-const _defultMessage = 'Search for the movie title'
+const _defultMessage = 'Search for the movie title';
 
 export default {
   // module
@@ -73,7 +73,7 @@ export default {
             })
           }
         }
-      } catch (message) {
+      } catch ({ message }) {
         commit('updateState', {
           movies: [],
           message
@@ -110,23 +110,6 @@ export default {
   }
 };
 
-function _fetchMovie(payload) {
-  const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c'
-  const url = id 
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-    .then((result) => {
-      if (result.data.Error) {
-        reject(result.data.Error)
-      }
-      resolve(result)
-    })
-    .catch((error) => {
-      reject(error.message)
-    })
-  })
+async function _fetchMovie(payload) {
+  return await axios.post('/.netlify/functions/movie', payload)
 }
