a6685578dc2574d6393f7e7dee63b93d7fd49654
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-02-21 16:14:36 +0900

    Add Test v1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes/index.js'; //index.js 생략 가능
 import store from './store/index.js'; //index.js 생략 가능
-import loadImage from './plugins/loadImages.js'
+import loadImage from './plugins/loadImages.js';
 
 createApp(App)
   .use(router) // $route, $router
